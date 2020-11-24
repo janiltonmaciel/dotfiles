dcefd28e3e5a170609b83bacf859707fb5082e82
Janilton Maciel <janilton@gmail.com>
2020-11-24 17:26:30 -0300

    add config python

---
+++ python/conf.fish
@@ -0,0 +1,11 @@
+set -Ux PIP_DOWNLOAD_CACHE "$HOME/Library/Caches/pip-downloads"
+set -Ux PYTHONUNBUFFERED 1
+set -Ux PIP_REQUIRE_VIRTUALENV true
+# set -U VIRTUALFISH_PLUGINS "compat_aliases"
+# set -U VIRTUALFISH_COMPAT_ALIASES "True"
+# set -Ux VIRTUALENVWRAPPER_SCRIPT /usr/local/bin/virtualenvwrapper.sh
+# set -Ux VIRTUALENVWRAPPER_HOOK_DIR "$HOME/.virtualenvs"
+# set -Ux WORKON_HOME $VIRTUALENVWRAPPER_HOOK_DIR
+
+# Python (poetry)
+# set -U fish_user_paths $HOME/.poetry/bin $fish_user_paths
