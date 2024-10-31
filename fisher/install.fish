36b4db3f685cc402ae3032f3c8b02c6e82a175d1
Janilton Maciel <janilton@gmail.com>
2024-10-31 14:47:07 -0300

    add fisher

---
+++ fisher/install.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+#
+# https://github.com/jorgebucaran/awsm.fish#readme
+# https://github.com/IlanCosman/tide
+# https://draculatheme.com/fish
+# https://github.com/jorgebucaran/fisher
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/nvm.fish
+fisher install IlanCosman/tide@v6
+fisher install dracula/fish
+
